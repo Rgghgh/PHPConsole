805c5c4f901fbefe4786ffb3494b3a9a4900393e
Rgghgh <tomer1091@gmail.com>
2017-08-18 21:00:22 +0300

    tmp quick

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+ace.require("ace/ext/language_tools");
 var editor = ace.edit("editor");
+var response = document.getElementById('response');
 editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/javascript");
+editor.getSession().setMode("ace/mode/php");
+editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true
+});
+
+editor.insert("<?php\n\n");
+editor.focus();
+
+editor.on('change', function() {
+    $.post('run.php', {'php': editor.getValue()}, function (r) {
+        response.innerHTML = ('<iframe>' + r + '</iframe>');
+    })
+});
