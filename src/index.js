072efe1e0e231a2ffb42575ae7df60b365b53feb
Rgghgh <tomer1091@gmail.com>
2017-08-18 19:41:47 +0300

    probably giving up react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {CodeEditor} from "./CodeEditor";
 
 class App extends React.Component {
 
     render() {
-
+        return (<CodeEditor/>)
     }
 }
 
