30a826fd7bf2bc8e1f13fbc13984dbe749f8f2b1
Rgghgh <tomer1091@gmail.com>
2017-08-18 18:22:18 +0300

    setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-/**
- * Created by tomer on 18-08-17.
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+    render() {
+        return <h1>Hello</h1>
+    }
+}
+
+ReactDOM.render(<Hello/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-const path = require('path');
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
     entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+    output: { path: __dirname, filename: 'dist/bundle.js' },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
 };
