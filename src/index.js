dc4e610ac841e9b8bb3c4cdd1eaaf427f4f2e197
Rgghgh <tomer1091@gmail.com>
2017-08-18 19:09:23 +0300

    removed react-ace

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
-import brace from 'brace';
-import AceEditor from 'react-ace';
-
-import 'brace/mode/php';
-import 'brace/theme/github';
-
-function onChange(newValue) {
-    console.log('change', newValue);
-}
 
 class App extends React.Component {
+
     render() {
-        return <AceEditor
-            mode="php"
-            theme="github"
-            onChange={onChange}
-            name="code"
-            editorProps={{$blockScrolling: true, $enableBasicAutocompletion: true, $enableLiveAutocompletion:true}}
-        />
+
     }
 }
 
