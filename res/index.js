b1e69939332851946680496ae49a7e103a699c29
Rgghgh <tomer1091@gmail.com>
2017-08-19 21:09:04 +0300

    cdn removed, libs are local

--- res/index.js
+++ res/index.js
@@ -13,6 +13,6 @@ editor.focus();
 
 editor.on('change', function() {
     $.post('run.php', {'php': editor.getValue()}, function (r) {
-        response.innerHTML = r;
+        response.innerText = r;
     })
 });
--- run.php
+++
@@ -1,6 +0,0 @@
-<?php
-try {
-    echo eval('?>' . $_POST['php']);
-} catch (Exception $e) {
-    echo 'Invalid Syntax';
-}
